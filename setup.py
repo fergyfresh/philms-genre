7b4b351598384af1c9fb42289d6c1bb31ac75c41
Billy Ferguson <wferguson@blueprintpower.com>
2019-11-15 22:28:31 -0500

    Adds some helper functions to setup

--- setup.py
+++ setup.py
@@ -16,6 +16,38 @@ def get_version(package):
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
 
 
+def read(*paths):
+    """
+    Build a file path from paths and return the contents.
+    """
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
 name = 'philms-genre'
 version = get_version('philms_genre')
 package = 'philms_genre'
