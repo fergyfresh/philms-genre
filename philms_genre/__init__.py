4e83b8d44f25e708ef06239983e90b8e60fc7628
Billy Ferguson <wferguson@blueprintpower.com>
2019-11-15 22:20:15 -0500

    Adds info to package init for versioning and whatnot

---
+++ philms_genre/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'philms_genre'
+__version__ = '0.1'
+__author__ = 'Billy Ferguson'
+__license__ = 'WTFPL'
+__copyright__ = 'Copyright 2019 Ferg Self Co'
+
+# Version synonym
+VERSION = __version__
